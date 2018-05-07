103810e7ffdf12e8da9d8cd127b11ced0572a112
rafael francisco <broncasrafa@gmail.com>
2018-05-07 09:57:38 -0300

    adicionar a sua propria api-key

--- src/app/app.marvel.settings.ts
+++ src/app/app.marvel.settings.ts
@@ -1,6 +1,6 @@
 export const marvelAppSettings = {
   marvel_endpoint: 'https://gateway.marvel.com:443/v1/public',
-  marvel_apiKey: '514a15d713cfbc064c23ea69208462ad',
+  marvel_apiKey: 'your-api-key',
   marvel_teste_ts: '1501611177',
   marvel_teste_hash: '7ac19c7737cf21ecc4bb9c5d9a3c3c60'
 };
